3a8d287bf669e97b4dfc7f1378e30297486f623d
Gunnar Geir Helgason <gunnargeir89@gmail.com>
2019-04-08 10:58:39 +0200

    Add files via upload

---
+++ components/queries/gqlquery.js
@@ -0,0 +1,33 @@
+import gql from 'graphql-tag';
+
+const gqlQueries = () => (
+GET_PRODUCTS_BY_ID = gql`
+  query getProducts($ids: [ID!]!) {
+    nodes(ids: $ids) {
+      ... on Product {
+        title
+        handle
+        descriptionHtml
+        id
+        images(first: 1) {
+          edges {
+            node {
+              originalSrc
+              altText
+            }
+          }
+        }
+        variants(first: 1) {
+          edges {
+            node {
+              price
+              id
+            }
+          }
+        }
+      }
+    }
+  }
+`
+)
+export default gqlQueries
